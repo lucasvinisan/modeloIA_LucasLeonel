f30d502027821080c90903b1b93bd0d36deaaaba
lucasvinisan <lucasviniciuslvsl118@gmail.com>
2026-04-17 13:20:57 -0300

    Enviando a implementação do model.tflite e optimize_model.py

--- optimize_model.py
+++ optimize_model.py
@@ -2,3 +2,21 @@ import tensorflow as tf
 import os
 
 #insira seu código aqui
+
+#Crregando o modelo CNN criado 
+model = tf.keras.models.load_model('mnist_model.h5')
+
+#Conversor
+converter_model = tf.lite.TFLiteConverter.from_keras_model(model)
+
+#Otimização 
+converter_model.optimizations = [tf.lite.Optimize.DEFAULT]
+
+tflite_modelo = converter_model.convert()
+
+
+#Salvando os arquivos 
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_modelo)
+
+print('Sucesso conversão')
